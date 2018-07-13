159fd211250c0212d818ac0d201b835bc94f221b
banspach17 <banspach@altair.com>
2018-07-13 13:29:50 -0400

    update get_third_party_linux.sh display curl -v

--- get_third_party_linux.sh
+++ get_third_party_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-set -x
 #curl -ubanspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
-curl -u banspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
+set -x
+curl -v -u banspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
+set +x
