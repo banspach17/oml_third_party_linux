62d2925f797bba6d8fe51ace218d41782da998a4
banspach17 <banspach@altair.com>
2018-07-13 13:37:07 -0400

    update get_third_party_linux.sh use ip address

--- get_third_party_linux.sh
+++ get_third_party_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #curl -ubanspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
+#curl -v -u banspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
 set -x
-curl -v -u banspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
+curl -v -u banspach:Hgrth67@ -O "http://10.130.2.28:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
 set +x
