23caa07ce32d784837195e3642cf9d9022352e72
banspach17 <banspach@altair.com>
2018-07-18 16:45:16 -0400

    attempt ftp again

--- get_third_party_linux.sh
+++ get_third_party_linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #curl -ubanspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
 #curl -v -u banspach:Hgrth67@ -O "http://dave-test.prog.altair.com:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
+#Below worked but the website was not visible outside firewall.
+#curl -v -u banspach:Hgrth67@ -O "http://10.130.2.28:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
 set -x
-curl -v -u banspach:Hgrth67@ -O "http://10.130.2.28:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
+curl -v -u openmatrixthirdparty:EfWb[RMP2GuRs^LHfna4 -O "ftp://ftp2.altair.com/%2foutgoing/third_party_linux.tar.gz"
 set +x
