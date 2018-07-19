442b72be35b5a1376535a9656cdf99001a9d0d37
banspach17 <banspach@altair.com>
2018-07-19 11:25:54 -0400

    update curl command --disable-eprt

--- get_third_party_linux.sh
+++ get_third_party_linux.sh
@@ -4,9 +4,9 @@
 #Below worked but the website was not visible outside firewall.
 #curl -v -u banspach:Hgrth67@ -O "http://10.130.2.28:8081/artifactory/generic-local/dbagent-4.4.3.536.zip"
 #curl -v -u openmatrixthirdparty:EfWb[RMP2GuRs^LHfna4 -O "ftp://ftp2.altair.com/%2foutgoing/third_party_linux.tar.gz"
+#The command below works, but tries a couple of FTP pulls which are not compatible with the available server.
+#curl -v -u openmatrixthirdparty:EfWb[RMP2GuRs^LHfna4 -O "ftp://ftp2.altair.com/%2foutgoing/smallfile.tar.gz"
 set -x
-curl -v -u openmatrixthirdparty:EfWb[RMP2GuRs^LHfna4 -O "ftp://ftp2.altair.com/%2foutgoing/smallfile.tar.gz"
-curl -v -u openmatrixthirdparty:EfWb[RMP2GuRs^LHfna4 --ftp-port 21 -O "ftp://ftp2.altair.com/%2foutgoing/smallfile.tar.gz"
-
+curl -v -u openmatrixthirdparty:EfWb[RMP2GuRs^LHfna4 --disable-eprt -O "ftp://ftp2.altair.com/%2foutgoing/smallfile.tar.gz"
 set +x
 curl --help
